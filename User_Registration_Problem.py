9e9d2ff9132048b656794c54214f62f8df6f1ffa
[Tejas Tonpe] <[tejastonpe.12345@gmail.com]>
2024-05-02 15:16:55 +0530

    [Tejas Tonpe] Add: Created UC3 of user registration problem which validates email address using regex

--- User_Registration_Problem.py
+++ User_Registration_Problem.py
@@ -4,26 +4,38 @@ import logging
 logger=logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s %(message)s',filename="user_registration.log",level=logging.INFO)
 
-def first_name(f_name):
+def validate_first_name(first_name):
     '''
     Description : Validate the input string as a first name.
-    Args    : first_name (str)-input string to be validated.
-    Returns : bool: True if the input string is a valid first name else False.
+    Parameter   : first_name (str)-input string to be validated.
+    Returns     : bool- True if the input string is a valid first name else False.
     '''
     pattern = r'^[A-Z][a-z]{2,}$'
-    if re.match(pattern, f_name):
+    if re.match(pattern, first_name):
         return True
     else:
         return False
 
-def last_name(l_name):
+def validate_last_name(last_name):
     '''
     Description : Validate the input string as a last name.
-    Args    : last_name(str)-input string to be validated.
-    Returns : bool: True if the input string is a valid first name else False.
+    Parameter   : last_name(str)-input string to be validated.
+    Returns     : bool- True if the input string is a valid first name else False.
     '''
     pattern = r'^[A-Z][a-z]{2,}$'
-    if re.match(pattern,l_name):
+    if re.match(pattern,last_name):
+        return True
+    else:
+        return False
+
+def validate_email(email_address):
+    '''
+    Description : Validate the input string as a email.
+    Parameter   : inp_email - input to be validated.
+    Returns     : bool- True if the input string is a valid email address else False.
+    '''
+    pattern = r'^\w+\.+\w+@\w+\.+\w+$'
+    if re.match(pattern,email_address):
         return True
     else:
         return False
@@ -31,13 +43,15 @@ def last_name(l_name):
 def main():
     """
     Main function to get user input 
-    returns : None.
+    returns : None
     """
     try:
-        f_name=input("Enter your first name: ")
-        l_name=input("Enter your last name: ")
-        print(first_name(f_name))
-        print(last_name(l_name))
+        first_name=input("Enter your first name: ")
+        last_name=input("Enter your last name: ")
+        email_address=input("Enter your email: ")
+        print(validate_first_name(first_name))
+        print(validate_last_name(last_name))
+        print(validate_email(email_address))
     except Exception as e:
         logger.error("Error occourd: %s",e)
 
