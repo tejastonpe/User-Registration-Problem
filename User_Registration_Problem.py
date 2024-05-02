58e97c26b45dbd828e3f40e68630ffaacb1e38ae
[Tejas Tonpe] <[tejastonpe.12345@gmail.com]>
2024-05-02 14:31:34 +0530

    [Tejas Tonpe] Add: Created UC2 of user registration problem  which validate last name using regex

--- User_Registration_Problem.py
+++ User_Registration_Problem.py
@@ -4,14 +4,26 @@ import logging
 logger=logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s %(message)s',filename="user_registration.log",level=logging.INFO)
 
-def first_name(inp):
+def first_name(f_name):
     '''
     Description : Validate the input string as a first name.
-    Args    : inp (str)-input string to be validated.
+    Args    : first_name (str)-input string to be validated.
     Returns : bool: True if the input string is a valid first name else False.
     '''
     pattern = r'^[A-Z][a-z]{2,}$'
-    if re.match(pattern,inp):
+    if re.match(pattern, f_name):
+        return True
+    else:
+        return False
+
+def last_name(l_name):
+    '''
+    Description : Validate the input string as a last name.
+    Args    : last_name(str)-input string to be validated.
+    Returns : bool: True if the input string is a valid first name else False.
+    '''
+    pattern = r'^[A-Z][a-z]{2,}$'
+    if re.match(pattern,l_name):
         return True
     else:
         return False
@@ -22,8 +34,10 @@ def main():
     returns : None.
     """
     try:
-        inp=input("Enter your first name: ")
-        print(first_name(inp))
+        f_name=input("Enter your first name: ")
+        l_name=input("Enter your last name: ")
+        print(first_name(f_name))
+        print(last_name(l_name))
     except Exception as e:
         logger.error("Error occourd: %s",e)
 
